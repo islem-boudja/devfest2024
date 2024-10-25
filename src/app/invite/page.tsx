ce5044c7d820a7cc6a37d989458e30b38f0c37f5
islem-boudja <i_boudja@estin.dz>
2024-10-25 09:11:24 +0100

    Refactor invite page component and update callback URL

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -2,7 +2,7 @@ import { getServerAuthSession } from "~/server/auth";
 import { redirect } from "next/navigation";
 import SignInButton from "~/app/_components/invite-button";
 import { api } from "~/trpc/server";
-import { env } from "~/env";
+
 export default async function SignInPage({
   searchParams,
 }: {
@@ -11,23 +11,32 @@ export default async function SignInPage({
   const session = await getServerAuthSession();
   const inviteToken = searchParams.invite;
 
-  if (session && session.user.organizationId) {
-  }
-  let callbackurl;
-  if (session && inviteToken) {
-    const addManager = await api.organization.addManager({
-      userId: session.user.id,
-      token: inviteToken,
-    });
-    console.log(addManager);
+  if (session?.user?.organizationId) {
     redirect("/dashboard");
   }
-  if (inviteToken) {
-    callbackurl = `${env.NEXTAUTH_URL}/organization/invite?invite=${inviteToken}`;
+
+  if (session && inviteToken) {
+    const addManager = await api.organization.addManager({
+      userId: session?.user?.id,
+      token: inviteToken,
+    });
+    console.log("addManager: ", addManager);
+    redirect("/dashboard");
   }
-  callbackurl = `${env.NEXTAUTH_URL}/create-organization`;
+
+  const callbackUrl = `/invite?invite=${inviteToken}`;
+  //   if (inviteToken) {
+  //     callbackUrl = `/invite?invite=${inviteToken}`;
+  //   }
+
+  //   callbackUrl = "/dashboard";
   return (
     <div className="mx-auto max-w-md p-6">
+      {inviteToken && session && (
+        <div className="mb-4 rounded-md bg-blue-50 p-3 text-blue-700">
+          You are being linked to an org
+        </div>
+      )}
       <h1 className="mb-6 text-2xl font-bold">Sign In</h1>
 
       {inviteToken && (
@@ -36,7 +45,7 @@ export default async function SignInPage({
         </div>
       )}
 
-      <SignInButton callbackurl={callbackurl} />
+      <SignInButton callbackurl={callbackUrl} />
     </div>
   );
 }
